73ba143743d6bae58229c4d06c68a3ba40ce678c
Diego Lucas <diego.lucasilva@gmail.com>
2021-01-16 17:56:42 -0300

    close redis connection

--- python_course/redis/redisclient.py
+++ python_course/redis/redisclient.py
@@ -9,27 +9,28 @@ CHUNK_SIZE = 2
 def scanClean():
     for connection in connections:
         print(connection)
-        client = redis.Redis(host=connection['host'], port=connection['port'],db=connection['db'])
+        cache = redis.Redis(host=connection['host'], port=connection['port'],db=connection['db'])
         mock()
-        clean("*", client, connection)
+        clean("*", cache, connection)
 
-def clean(pattern, client, connection):
+def clean(pattern, cache, connection):
     cursor = '0'
     deleted_count = 0
     while cursor != 0:
-        cursor, keys = client.scan(cursor=cursor, match=pattern, count=CHUNK_SIZE)
+        cursor, keys = cache.scan(cursor=cursor, match=pattern, count=CHUNK_SIZE)
         if keys:
-            deleted_count += client.delete(*keys)
-        
+            deleted_count += cache.delete(*keys)
+    
+    cache.close()
     print('{} registries deleted from: {} DB {}'.format(deleted_count,connection['host'],connection['db']))
 
 def mock():
-    client = redis.Redis(host='localhost', port=6379,db=0)
+    cache = redis.Redis(host='localhost', port=6379,db=0)
     for key, value in (('A', '1'), ('B', '2'), ('C', '3'),('D', '3')):
-        client.set(key, value)
+        cache.set(key, value)
 
-    client2 = redis.Redis(host='localhost', port=6379,db=1)
+    cache2 = redis.Redis(host='localhost', port=6379,db=1)
     for key, value in (('F', '1'),('G', '1')):
-        client2.set(key, value)
+        cache2.set(key, value)
 
 scanClean()
